19d13310e78ebeaf793465f735e5fd76e17d35dc
aldjanaliev <aldjanaliev@gmail.com>
2023-04-24 10:57:19 +0600

    added slider nav and tab

--- js/core.js
+++ js/core.js
@@ -1,21 +1,31 @@
 $(document).ready(function() {
 	// ====== inputmask ======
 
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
-    
+  $('.slider-for').each(function(i, e){
+    let slideTab = $(e).closest('.slider_wrap').find('.slider-nav')
+    $(e).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: slideTab
+    });
+  })
+  $('.slider-nav').each(function(i, e){
+    let slideFor = $(e).closest('.slider_wrap').find('.slider-for')
+    let slidesToShow = 4
+    if($(e).closest('.recommend').length != 0){
+      slidesToShow = 3
+    }
+    $(e).slick({
+      slidesToShow: slidesToShow,
+      slidesToScroll: 1,
+      asNavFor: slideFor,
+      arrows: false,
+      dots: false,
+      focusOnSelect: true
+    });
+  })
+
 
 
 })
