1d91ecf9fba2a42febdd1abb4409d9d7eb7d3631
aldjanaliev <aldjanaliev@gmail.com>
2023-04-28 11:00:04 +0600

    added imgs, tag, and some stles

--- js/core.js
+++ js/core.js
@@ -41,4 +41,13 @@ $(document).ready(function() {
   })
 
 
+  $('.scroll').on('click', function(){
+    let href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+  })
+
+
 })
